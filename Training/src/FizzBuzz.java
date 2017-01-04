2a40b976c1ad708d0d015830cb9186062d532a34
tomatomaster <iiitomatomasteriii@gmail.com>
2017-01-04 19:58:09 +0900

    FizzBuzz

--- Training/src/FizzBuzz.java
+++ Training/src/FizzBuzz.java
@@ -5,11 +5,12 @@
  */
 public class FizzBuzz {
   public static void main(String[] args) {
-    new FizzBuzz().fizzbuzz(args);
+    int value = Integer.valueOf(args[0]);
+    new FizzBuzz().fizzbuzz(value);
   }
 
-  public void fizzbuzz(String[] args) {
-    for (int i = 1; i <= Integer.valueOf(args[0]); i++) {
+  public void fizzbuzz(int value) {
+    for (int i = 1; i <= value; i++) {
       if (i % 15 == 0) {
         System.out.println("FizzBuzz");
       } else if (i % 5 == 0) {
